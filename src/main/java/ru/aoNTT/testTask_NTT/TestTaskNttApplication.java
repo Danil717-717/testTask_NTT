cb365e894f41338434bff8ae22891534ef37ac85
danil <danila_monster@mail.ru>
2024-07-10 17:16:46 +0300

    addLiquibase

--- src/main/java/ru/aoNTT/testTask_NTT/TestTaskNttApplication.java
+++ src/main/java/ru/aoNTT/testTask_NTT/TestTaskNttApplication.java
@@ -2,8 +2,10 @@ package ru.aoNTT.testTask_NTT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "ru.aoNTT.testTask_NTT.models.entity")
 public class TestTaskNttApplication {
 
 	public static void main(String[] args) {
